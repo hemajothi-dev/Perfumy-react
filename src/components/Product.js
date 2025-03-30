0141041e504486a1a1c3c6656a5026df1d60a188
hema2423 <hemajothishankar@gmail.com>
2025-03-30 20:05:59 +0530

    update react

---
+++ src/components/Product.js
@@ -0,0 +1,142 @@
+import one from "../asset/images/p-1.jpg";
+import two from "../asset/images/p-2.jpg";
+import three from "../asset/images/p-3.jpg";
+
+
+function Product(){
+    return(
+      <div class="products">
+              <div class="box">
+                  <img src={one} alt="perfume-img"></img>
+                   <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>    
+              </div>
+                 
+              <div class="box">
+                  <img src={two} alt="perfume-img"></img>
+                  <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p> 
+              </div>
+              <div class="box">
+                  <img src={three} alt="perfume-img"
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  ></img>
+                  <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>
+              </div>
+          </div>
+    )
+  }
+  
+ 
+  export default Product
+  
